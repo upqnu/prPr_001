31d0f927fd30eb8a016354fb10fbf600c81d0818
upqnu <upgrdrqnu@gmail.com>
2022-11-23 18:48:38 +0900

    Initial commit

---
+++ src/main/java/upqnu/prPr/Hello.java
@@ -0,0 +1,10 @@
+package upqnu.prPr;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hello {
+    private String data;
+}
---
+++ src/test/java/upqnu/prPr/PrPrApplicationTests.java
@@ -0,0 +1,13 @@
+package upqnu.prPr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrPrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
