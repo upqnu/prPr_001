2514bdc969d63dc7a968edb506d2660eaa2c64b3
upqnu <upgrdrqnu@gmail.com>
2022-12-12 16:56:28 +0900

    #221212_03 http request paramater - @RequestParam - required, default, map 조건 추가

--- src/main/java/upqnu/prPr/check/requestmapping/RequestParamController.java
+++ src/main/java/upqnu/prPr/check/requestmapping/RequestParamController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -50,10 +51,39 @@ public class RequestParamController {
 
     @ResponseBody
     @RequestMapping("/request-param-v4")
-    public String requestParam4(String email, String password) {
+    public String requestParam4(String email, String password) { // HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능
         log.info("email={}, password={}", email, password);
 
         return "OK. 개인프로젝트 관리 서비스 prPr를 위한 요청 파라미터 조회 테스트4입니다.";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String email, // required = true가 default. true인데 입력하지 않으면 400 에러 발생
+            @RequestParam(required = false) String password) { // required = false 이면 입력하지 않아도 에러가 발생하지 않음
+        log.info("email={}, password={}", email, password);
+
+        return "OK. 개인프로젝트 관리 서비스 prPr를 위한 요청 파라미터 -필수 여부 조회 테스트입니다.";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String email,
+            @RequestParam(required = false, defaultValue = "carpe diem") String password) {
+        log.info("email={}, password={}", email, password);
+
+        return "OK. 개인프로젝트 관리 서비스 prPr를 위한 요청 파라미터 - default 설정 조회 테스트입니다.";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(
+            @RequestParam Map<String, Object> paramMap) {
+        log.info("email={}, password={}", paramMap.get("email"), paramMap.get("password"));
+
+        return "OK. 개인프로젝트 관리 서비스 prPr를 위한 요청 파라미터 - map으로 조회 테스트입니다.";
     }
 
 }
