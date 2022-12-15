545054dd2050cca5191ccb4f6102fd8b39cedb76
upqnu <upgrdrqnu@gmail.com>
2022-12-15 23:00:03 +0900

    #221215_01 JpaReposiory 구현 없이 MemberMvcRepository(class) 생성 & 스프링 없이 MemberMvcRepositoryTest 생성

--- src/main/java/upqnu/prPr/member/repository/MemberRepository.java
+++ src/main/java/upqnu/prPr/member/repository/MemberRepository.java
@@ -3,10 +3,12 @@ package upqnu.prPr.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import upqnu.prPr.member.entity.Member;
 
 import java.util.List;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByNameAndEmail(String name, String email);
